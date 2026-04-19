46293a40401696b6053a618826386705124744ac
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-04-19 02:42:52 -0400

    Update esbuild.config.mjs

--- src/esbuild.config.mjs
+++ src/esbuild.config.mjs
@@ -1,24 +1,28 @@
 import { build } from "esbuild";
 import { nodeModulesPolyfillPlugin } from "esbuild-plugins-node-modules-polyfill";
 
-await build({
-  entryPoints: ["src/index.js"],
-  outfile: "dist/jest.umd.js",
-  bundle: true,
-  format: "esm",
-  globalName: "browserJest",
-  platform: "browser",
-  target: "es2022",
-  minify: true,
+async function run() {
+  await build({
+    entryPoints: ["src/index.js"],
+    outfile: "dist/jest.umd.js",
+    bundle: true,
+    format: "esm",
+    globalName: "browserJest",
+    platform: "browser",
+    target: "es2022",
+    minify: true,
 
-  define: {
-    global: "window",
-    "process.env.NODE_ENV": '"production"',
-  },
+    define: {
+      global: "window",
+      "process.env.NODE_ENV": '"production"',
+    },
 
-  plugins: [
-    nodeModulesPolyfillPlugin({
-      globals: { process: true, Buffer: true },
-    }),
-  ],
-});
+    plugins: [
+      nodeModulesPolyfillPlugin({
+        globals: { process: true, Buffer: true },
+      }),
+    ],
+  });
+}
+
+run();
